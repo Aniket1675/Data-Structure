5723137c1cb17ea13c983e56183d828ede3a063f
Aniket1675 <Aniket@05>
2022-03-25 08:54:00 +0530

    Added Data structure Programs

--- src/com/bridgelabz/datastructure/Linklist/LinkedList.java
+++ src/com/bridgelabz/datastructure/Linklist/LinkedList.java
@@ -1,157 +1,143 @@
 package com.bridgelabz.datastructure.Linklist;
 
-public class LinkedList
-{
-    //Node class.
-    static class Node{
-        int key;   //data value.
-        Node next; //next Pointer.
+public class LinkedList {
+    public Node head;  //head of the linked list. i.e first element.
+    public int size;   //number of elements in the list.
 
-        public Node(int data){
-            this.key=data;
-            this.next=null;
-        }
+
+    public LinkedList() {
+        head = null;
+        size = 0;
     }
 
-    static Node head;  //head of the linked list. i.e first element.
-    static int size;   //number of elements in the list.
+    //main functon to run the program.
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFront(10);
+        list.addBack(12);
 
+        list.display();
 
-    public LinkedList(){
-        head=null;
-        size=0;
+        list.addFront(8);
+        list.addFront(6);
+        list.addBack(20);
+
+        list.display();
+
+        list.delete(3);
+        list.display();
+
+        list.deleteFront();
+        list.display();
+
+        list.deleteBack();
+        list.display();
     }
 
     //function to add elements at the front of the list.
-    static public void pushFront(int number){
+    public void addFront(int number) {
         System.out.println("Inserting node at front : " + number);
         Node node = new Node(number);
-        if(head==null){
-            head=node;
+        if (head == null) {
+            head = node;
             size++;
             return;
         }
 
-        node.next=head;
-        head=node;
+        node.next = head;
+        head = node;
         size++;
     }
 
     //function to add elements at the back of the list.
-    static public void pushBack(int number){
+    public void addBack(int number) {
         System.out.println("Inserting node at back  : " + number);
         Node node = new Node(number);
-        if(head==null){
-            head=node;
+        if (head == null) {
+            head = node;
             size++;
             return;
         }
-        Node temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
         }
-        temp.next=node;
+        temp.next = node;
         size++;
     }
 
     //function to remove element from the front of the list.
-    static public void popFront(){
-        System.out.println("PopFront Operation.");
-        if(head==null){
+    public void deleteFront() {
+        System.out.println("delete front Operation.");
+        if (head == null) {
             return;
         }
 
-        head=head.next;
+        head = head.next;
         size--;
     }
 
     //function to remove elements from the back of the list.
-    static public void popBack(){
-        System.out.println("PopBack Operation.");
-        if(head==null){
+    public void deleteBack() {
+        System.out.println("delete back Operation.");
+        if (head == null) {
             return;
         }
 
-        if(head.next==null){
-            head=null;
+        if (head.next == null) {
+            head = null;
             size--;
             return;
         }
 
-        Node temp=head;
-        while(temp.next.next!=null){
-            temp=temp.next;
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
         }
-        temp.next=null;
+        temp.next = null;
         size--;
     }
 
     //function to remove element at a specific position.
-    static public void pop(int position){
-        System.out.println("Poping element at pos : " + position);
-        if(head==null){
+    public void delete(int position) {
+        System.out.println("delete element at position : " + position);
+        if (head == null) {
             return;
         }
-        if(position==0){
-            head=head.next;
+        if (position == 0) {
+            head = head.next;
             size--;
             return;
         }
 
-        Node temp=head;
-        int x=0;
-        while(temp.next!=null && x<position-1){
-            temp=temp.next;
+        Node temp = head;
+        int x = 0;
+        while (temp.next != null && x < position - 1) {
+            temp = temp.next;
             x++;
         }
 
-
-
-        if(temp.next==null){
+        if (temp.next == null) {
             return;
         }
 
-        temp.next=temp.next.next;
+        temp.next = temp.next.next;
         size--;
     }
 
     //function to display the list.
-    static public void display(){
+    public void display() {
         System.out.print("LinkedList : ");
-        if(head==null){
+        if (head == null) {
             System.out.println(" ");
             return;
         }
 
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.key +  " ");
-            temp=temp.next;
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.key + " ");
+            temp = temp.next;
         }
         System.out.println();
-    }
-
-    //main functon to run the program.
-    public static void main(String [] args){
-        LinkedList list = new LinkedList();
-        list.pushFront(10);
-        list.pushBack(12);
-
-        list.display();
-
-        list.pushFront(8);
-        list.pushFront(6);
-        list.pushBack(20);
-
-        list.display();
-
-        list.pop(3);
-        list.display();
-
-        list.popFront();
-        list.display();
-
-        list.popBack();
-        list.display();
     }
 }
---
+++ src/com/bridgelabz/datastructure/Linklist/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.datastructure.Linklist;
+
+public class Node{
+        int key;   //data value.
+        Node next; //next Pointer.
+
+        public Node(int data){
+            this.key=data;
+            this.next=null;
+        }
+}
+
+
